784998cd670723f2f93488100a1fd892c8da7d14
huy2222 <huynhatnguyen246@gmail.com>
2022-09-28 05:42:48 +0700

    my change

--- main.js
+++ main.js
@@ -50,64 +50,77 @@ const selection=document.querySelector('.selection-overlay')
     }
     //gán sự kiện cho thẻ input
     input.addEventListener("keyup", myFunction);
-    
+// nhấn vào ẩn list tiềm kiếm
+var SearchList = document.querySelector('.search-list');
+function UnshowlistSr() {
+  SearchList.style.display = 'none';
+}    
+SearchList.addEventListener('click',UnshowlistSr)
 // dữ liệu lưu trữ
 var DatamemberArray = [
   {
     name: 'Trần Thái Vinh',
     star:3,
-    avata: "mr.png",
+    avatar: "mr.png",
     timeteach:"12 năm",
     specialize: "Vật Lí",
     facebook: "https://www.facebook.com/huy.nguyennhat.7359/",
     email: "nguyennhathuy21112005@gmail.com",
+    zalo: "0375506199",
     phonenumber:"0375506199",
-    zalo: "0375506199"
   },
   {
     name: 'Nguyễn Xuân Hùng',
     star:2,
-    avata: "mr.png",
+    avatar: "mr.png",
     timeteach:"6 năm",
     specialize: "Ngữ Văn",
     facebook: "https://www.facebook.com/huy.nguyennhat.7359/",
     email: "nguyennhathuy21112005@gmail.com",
+    zalo: "0355782783",
     phonenumber:"0355782783",
-    zalo: "0355782783"
   },
   {
     name: 'Hồ Đức Triều',
     star:1,
-    avata: "mr.png",
+    avatar: "mr.png",
     timeteach:"6 năm",
     specialize: "Toán",
     facebook: "https://www.facebook.com/huy.nguyennhat.7359/",
     email: "ductrieu@gmail.com",
+    zalo: "0903138189",
     phonenumber:"0903138189",
-    zalo: "0903138189"
   },
 
   
 ]
 function DaTamemberFn(data , element) {
   var memberstr = '';
+  var BackgroundStar='';
     for (var i = 0; i<data.length; i++) {
       var ManyStar='';
     for (var s = 0; s< data[i].star; s++) {
       ManyStar += '<i class="star ti-star"></i>';
     }
+    if (data[i].star==3 & data[i].name =='Trần Thái Vinh') {
+      BackgroundStar='style="background-color:yellow;"';
+    } else {
+      BackgroundStar='style="background-color:red;"'
+    }
+
+    
       memberstr += `
       <div class="modal-container">
             <div class="modal-heading">   
                 <div class="number"></div>                                 
                 <h1 id="${data[i].name.toLowerCase().replace(/\s+/g, '')}">${data[i].name}</h1> 
-                <div class="star-position" style="background-color:red ;">
+                <div class="star-position" ${BackgroundStar}>
                   ${ManyStar}
                 </div>
                    
             </div>
             <div class="modal-content">
-                <img src="./asset/img/avata/${data[i].avata}" alt="">                
+                <img  onerror="this.src='./asset/img/avatar/mr_mrs.png'" src="./asset/img/avatar/${data[i].avatar}" alt="">                
             </div>
             <div class="modal-footer">                       
                 <p class="footer-content">Thời gian giảng dạy: ${data[i].timeteach}</p>
@@ -139,20 +152,20 @@ function DaTamemberFn(data , element) {
     `
   }
     element.innerHTML = memberstr;
-     
+
 }  
 var ModalJs = document.getElementById('modal-js');
  DaTamemberFn(DatamemberArray,ModalJs )  
 
 
-function SearchListFn(data , element) {
-  var searchstr = '';
-    for (var i = 0; i<data.length; i++) {
-      searchstr += `
-        <li><a href="#${data[i].name.toLowerCase().replace(/\s+/g, '')}">${data[i].name}</a></li>
-      `
-    }
-    element.innerHTML = searchstr;
-}  
-var SearchListJs = document.getElementById('myUL')
-SearchListFn(DatamemberArray,SearchListJs )  
+// function SearchListFn(data , element) {
+//   var searchstr = '';
+//     for (var i = 0; i<data.length; i++) {
+//       searchstr += `
+//         <li><a href="#${data[i].name.toLowerCase().replace(/\s+/g, '')}">${data[i].name}</a></li>
+//       `
+//     }
+//     element.innerHTML = searchstr;
+// }  
+// var SearchListJs = document.getElementById('myUL')
+// SearchListFn(DatamemberArray,SearchListJs )  
